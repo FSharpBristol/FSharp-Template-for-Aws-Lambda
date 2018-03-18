a75dd2650608686fa4b6bfd64540d714528a9a3a
Richard Harding <richard@sprydondesigns.com>
2018-03-18 14:30:45 +0000

    added netstandard 2.0 dotnet template

---
+++ content/FSharpTemplateforAwsLambda/script.fs
@@ -0,0 +1,10 @@
+namespace FSharpTemplateforAwsLambda
+
+module Program =
+    open System
+    open System.IO
+    open System.Text
+    open Amazon.Lambda.Core
+
+    let handler(context:ILambdaContext) = 
+        printfn "Hello World!"
